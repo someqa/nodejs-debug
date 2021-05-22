f696c9ec7ade0fbe342bf3d949c86c22574a2dba
unknown <olga.qa@yahoo.com>
2021-05-23 00:01:49 +0300

    chore: add eslint, remove unused var after eslint check with recommended config

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -28,7 +28,7 @@ router.get('/:id', (req, res) => {
                 })
             },
 
-            function findFail(err) {
+            function findFail() {
                 res.status(500).json({
                     message: "Data not found."
                 })
