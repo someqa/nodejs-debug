6f54273333b90fc7e6f59b46f7d90314e329fb85
unknown <olga.qa@yahoo.com>
2021-05-23 12:48:53 +0300

    chore: remove operator aliases warning (sequelize)

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    operatorsAliases: false
 })
 
 sequelize.authenticate().then(
